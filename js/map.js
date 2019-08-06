46a394403a18f6a08059d9a01809cba0356e8402
virgilxw <stupidpower95@gmail.com>
2019-08-06 13:21:03 +0800

    added control panel

--- js/map.js
+++ js/map.js
@@ -1,41 +1,36 @@
 var map = L.map('map').setView([1.35, 103.82], 12);
 
-var OneMapSG_Default = L.tileLayer('https://maps-{s}.onemap.sg/v3/Default/{z}/{x}/{y}.png', {
+// Basemaps
+var layerOneMapSG_Default = L.tileLayer('https://maps-{s}.onemap.sg/v3/Default/{z}/{x}/{y}.png', {
 	minZoom: 11,
 	maxZoom: 18,
 	bounds: [[1.56073, 104.11475], [1.16, 103.502]],
 	attribution: '<img src="https://docs.onemap.sg/maps/images/oneMap64-01.png" style="height:20px;width:20px;"/> New OneMap | Map data &copy; contributors, <a href="http://SLA.gov.sg">Singapore Land Authority</a>'
 });
 
-var basemaps = [
-	L.tileLayer('https://maps-{s}.onemap.sg/v3/Default/{z}/{x}/{y}.png', {
-		minZoom: 11,
-		maxZoom: 18,
-		bounds: [[1.56073, 104.11475], [1.16, 103.502]],
-		attribution: '<img src="https://docs.onemap.sg/maps/images/oneMap64-01.png" style="height:20px;width:20px;"/> New OneMap | Map data &copy; contributors, <a href="http://SLA.gov.sg">Singapore Land Authority</a>'
-	}),
-	    L.tileLayer('//{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
-		attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-		subdomains: 'abcd',
-		maxZoom: 20,
-		minZoom: 0,
-		label: 'Toner Lite' // optional label used for tooltip
-	}),
-	L.tileLayer('//{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
-		attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-		subdomains: 'abcd',
-		maxZoom: 20,
-		minZoom: 0,
-		label: 'Toner Lite' // optional label used for tooltip
-	})
+// Default base Map
+map.addLayer(layerOneMapSG_Default);
 
-];
+// Styled Layer Control
+var baseMaps = [
+	{
+		groupName: "Base Maps",
+		expanded: true,
+		layers: {
+			"OneMap": layerOneMapSG_Default,
+			"1953 Topological": layerTopo1953
+		}
+	}];
 
-map.addControl(L.control.basemaps({
-	basemaps: basemaps,
-	tileX: 1614, // tile X coordinate
-	tileY: 1016, // tile Y coordinate
-	tileZ: 11 // tile zoom level
-}));
+var overlays = [
+	];
 
-// TODO: fix thumbnails
+var styledLayerControlOptions = {
+	container_width: "300px",
+	container_maxHeight: "350px",
+	group_maxHeight: "80px",
+	exclusive: true
+};
+
+var styledLayerControl = L.Control.styledLayerControl(baseMaps, overlays, styledLayerControlOptions);
+map.addControl(styledLayerControl);
