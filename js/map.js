223299cf2b69792cbe24ac07bc46aa5830ce1972
virgilxw <stupidpower95@gmail.com>
2019-08-07 16:48:56 +0800

    Aside is collaspsable

--- js/map.js
+++ js/map.js
@@ -1,12 +1,24 @@
+// Resize aside
+var box = $("#resize").click(function(){
+	$(".stats").toggleClass("hidden");
+	console.log("triggered");
+});
+
+
+
 var map = L.map('map').setView([1.35, 103.82], 12);
 
 // Basemaps
-var layerOneMapSG_Default = L.tileLayer('https://maps-{s}.onemap.sg/v3/Default/{z}/{x}/{y}.png', {
+var LayerOneMapSG_Default = L.tileLayer('https://maps-{s}.onemap.sg/v3/Default/{z}/{x}/{y}.png', {
 	minZoom: 11,
 	maxZoom: 18,
 	bounds: [[1.56073, 104.11475], [1.16, 103.502]],
 	attribution: '<img src="https://docs.onemap.sg/maps/images/oneMap64-01.png" style="height:20px;width:20px;"/> New OneMap | Map data &copy; contributors, <a href="http://SLA.gov.sg">Singapore Land Authority</a>'
 });
+
+var LayerOSM = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+})
 
 var Layer1953Topo = L.WMS.tileLayer("https://libmaps.nus.edu.sg/gis/services/Sing_Hist_Maps/1953/MapServer/WMSServer?", {
 	"layers": "11,12,13,14,15,16,17,18,19,20,21,22",
@@ -19,10 +31,12 @@ var Layer1953Aerial = L.WMS.tileLayer("https://libmaps.nus.edu.sg/gis/services/S
 });
 
 // Default base Map
-map.addLayer(layerOneMapSG_Default);
+map.addLayer(LayerOSM);
 
 // Add geojson
-var Layer1955Wards = new L.GeoJSON.AJAX("data/wards1955.geojson");
+var Layer1955Wards = new L.GeoJSON.AJAX("data/wards1955.geojson", {
+	attribution: 'data.gov.sg'
+}).addTo(map);
 
 // Styled Layer Control
 var baseMaps = [
@@ -30,7 +44,8 @@ var baseMaps = [
 		groupName: "Base Maps",
 		expanded: true,
 		layers: {
-			"OneMap": layerOneMapSG_Default
+			"OneMap": LayerOneMapSG_Default,
+			"OSM": LayerOSM
 		}
 	}];
 
