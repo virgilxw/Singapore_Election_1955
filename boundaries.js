5eb61e5b49232ce78c131062bbea8ef0e0ec8488
virgilxw <stupidpower95@gmail.com>
2019-08-31 04:12:56 +0800

    profiles now fade in

--- boundaries.js
+++ boundaries.js
@@ -1,35 +1,63 @@
 $(document).ready(function () {
 
-
-    var controller = new ScrollMagic.Controller();
-
+	// init controller
+	var controller = new ScrollMagic.Controller();
+	
     // Fade in banner text at slightly different durations.
-    var bannerH1 = $("#banner .inner h1")
-    TweenMax.fromTo(bannerH1, 2, {
+    var bannerH1 = $("#banner .inner h1");
+    TweenMax.fromTo(bannerH1, 1, {
         opacity: 0,
-        x: -15
+        x: -20
     }, {
         opacity: 1,
         x: 0
-    })
-    var bannerp = $("#banner .inner p")
-    TweenMax.fromTo(bannerp, 5, {
+    });
+    var bannerp = $("#banner .inner p");
+    TweenMax.fromTo(bannerp, 2, {
         opacity: 0,
-        x: -15
+        x: -20
     }, {
         opacity: 1,
         x: 0
-    })
+    });
+	
+	// Fade in profiles
+	var officialProfileFadeIn = new ScrollMagic.Scene({
+		triggerElement: "#GGThomson",
+		triggerHook: 0.8, // Set position of trigger at 85% down the viewport
+		reverse: true
+	})
+	.setTween("#GGThomson", 1, {opacity: "1"} )
+	.addIndicators({name: "Official Profile Fade In"}) // add indicators (requires plugin)
+	.addTo(controller);
+	
+	var legCoProfileFadeIn = new ScrollMagic.Scene({
+		triggerElement: ".LegCo",
+		triggerHook: 0.8, // Set position of trigger at 85% down the viewport
+		reverse: true
+	})
+	.setTween(".LegCo", 1, {opacity: "1"} )
+	.addIndicators({name: "LegCo Profile Fade In"}) // add indicators (requires plugin)
+	.addTo(controller);
 
-    // Create scenes for revealing committee members
-
-    var officialFadeIn = ScrollMagic.Scene({
-								triggerElement: $("profileLi Official"), // y value not modified, so we can use element as trigger as well
-								offset: 5,												 // start a little later
-								triggerHook: 0.9,
-							})
-							.setClassToggle($("profile Official"), "visible") // add class toggle
-							.addIndicators({name: "digit " + (i+1) }) // add indicators (requires plugin)
-							.addTo(controller);
-
-})
+	var citCoProfileFadeIn = new ScrollMagic.Scene({
+		triggerElement: ".citCo",
+		triggerHook: 0.8, // Set position of trigger at 85% down the viewport
+		reverse: true
+	})
+	.setTween(".citCo", 1, {opacity: "1"} )
+	.addIndicators({name: "CitCo Profile Fade In"}) // add indicators (requires plugin)
+	.addTo(controller);
+	
+	var RurBoProfileFadeIn = new ScrollMagic.Scene({
+		triggerElement: ".citCo",
+		triggerHook: 0.8, // Set position of trigger at 85% down the viewport
+		reverse: true,
+		offset: 200
+	})
+	.setTween(".RurBo", 1, {opacity: "1"} )
+	.addIndicators({name: "CitCo Profile Fade In"}) // add indicators (requires plugin)
+	.addTo(controller);
+	
+	
+});
