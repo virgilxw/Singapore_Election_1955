a639da826655ce3b00652636262ede3eebb533ec
root <root@DESKTOP-LDIBQ8B.localdomain>
2019-09-07 02:11:23 +0800

    map is now pinned

--- boundaries.js
+++ boundaries.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
     // Fade in profiles
     var officialProfileFadeIn = new ScrollMagic.Scene({
             triggerElement: "#GGThomson",
-            triggerHook: 0.7, // Set position of trigger at 85% down the viewport
+            triggerHook: 0.5, // Set position of trigger at 85% down the viewport
             reverse: true
         }).setTween("#GGThomson", 1, {
             opacity: "1"
@@ -36,7 +36,7 @@ $(document).ready(function () {
         .addTo(controller);
     var legCoProfileFadeIn = new ScrollMagic.Scene({
             triggerElement: ".LegCo",
-            triggerHook: 0.7, // Set position of trigger at 85% down the viewport
+            triggerHook: 0.5, // Set position of trigger at 85% down the viewport
             reverse: true,
             offset: 100
         }).setTween(".LegCo", 1, {
@@ -53,7 +53,7 @@ $(document).ready(function () {
         .addTo(controller);
     var citCoProfileFadeIn = new ScrollMagic.Scene({
             triggerElement: ".citCo",
-            triggerHook: 0.7, // Set position of trigger at 85% down the viewport
+            triggerHook: 0.5, // Set position of trigger at 85% down the viewport
             reverse: true
         }).setTween(".citCo", 1, {
             opacity: "1"
@@ -69,7 +69,7 @@ $(document).ready(function () {
         .addTo(controller);
     var RurBoProfileFadeIn = new ScrollMagic.Scene({
             triggerElement: ".citCo",
-            triggerHook: 0.7, // Set position of trigger at 85% down the viewport
+            triggerHook: 0.5, // Set position of trigger at 85% down the viewport
             reverse: true,
             offset: 200
         }).setTween(".RurBo", 1, {
@@ -86,33 +86,66 @@ $(document).ready(function () {
         .addTo(controller);
 
     // Resize map div
-    function resizeMap(){
+    function resizeMap() {
         var vph = window.innerHeight
         $("#map").height(vph);
-        var vpw = $(window).width();
-        $("#map").width(vpw);
         console.log(vph)
     }
-    
+
+
     resizeMap();
-    $(window).resize(resizeMap).resize();
-    
+    $(window).resize(function () {
+        resizeMap()
+        setTimeout(function () {
+            map.invalidateSize()
+        }, 400);
+    });
+
     //Create scene to pin map
-    
-    var pinMap = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 300})
-						.setPin("#pin1")
-						.addIndicators({name: "1 (duration: 300)"}) // add indicators (requires plugin)
-						.addTo(controller);
+    var pinMap = new ScrollMagic.Scene({
+            triggerElement: "#map",
+            triggerHook: 0
+        })
+        .setPin("#map")
+        .addIndicators({
+            name: "pinMap"
+        })
+        .addTo(controller);
+
+    // Fixes map issues on startup
+    setTimeout(function () {
+        map.invalidateSize()
+    });
 
     // Initiates Map
-    var map = L.map('map').setView([1.35, 103.82], 12);
+    var map = L.map('map', {
+        zoomControl: false
+    }).setView([1.35, 103.82], 12);
     // Basemaps
     var LayerOneMapSG_Default = L.tileLayer('https://api.mapbox.com/styles/v1/virgilwxw/ck06ksmvx2axs1ctqea2zqt77/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidmlyZ2lsd3h3IiwiYSI6ImNqYmhrN25rZTNoNWgyeHBlNnY0N3Z6dDAifQ.KCzg-gN0vwIeQNoQyjWVXg', {
-        minZoom: 11,
-        maxZoom: 18,
-        bounds: [[1.56073, 104.11475], [1.16, 103.502]],
-        attribution: 'Mapbox'
+        attribution: '© <a href="https://apps.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 
+    map.setView([1.3521, 103.8198]).setZoom(11)
 
-    });
+    // Fade in profiles
+    var layer = new ScrollMagic.Scene({
+            triggerElement: "#GGThomson",
+            triggerHook: 0.5, // Set position of trigger at 85% down the viewport
+            reverse: true
+        }).setTween("#GGThomson", 1, {
+            opacity: "1"
+        }).on("enter", function () {
+            $("#listOfficial").css("background-color", "rgba(219, 211, 0, 0.22)")
+        }).on("leave", function () {
+            $("#listOfficial").css("background-color", "rgba(219, 211, 0 , 0)")
+        }).addIndicators({
+            name: "Official Profile Fade In"
+        }) // add indicators (requires plugin)
+        .addTo(controller);
+
+    var Layer1953 = L.WMS.tileLayer("https://libmaps.nus.edu.sg/gis/services/Sing_Hist_Maps/1953/MapServer/WMSServer?", {
+        "layers": "11,12,13,14,15,16,17,18,19,20,21,22",
+        attribution: '<a href="https://libmaps.nus.edu.sg/">National University of Singapore<a>'
+    }).addTo(map)
 });
